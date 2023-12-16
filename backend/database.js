16de23212d7313e80b8ffe37c526e79386495d2d
plavy <tinplavec@gmail.com>
2023-12-16 11:45:29 +0100

    all endpoints operational

--- backend/database.js
+++ backend/database.js
@@ -7,16 +7,33 @@ const pool = new Pool({
     database: 'or',
 })
 
-function isIncluded(element, query, field) {
+function isIncluded(element, query, field, exactMatch = false) {
     if (!field || field == 'wildcard') {
-        return JSON.stringify(element).toLowerCase().includes(query.toLowerCase());
+        if (exactMatch) {
+            return JSON.stringify(element).toLowerCase() == query.toLowerCase();
+        } else {
+            return JSON.stringify(element).toLowerCase().includes(query.toLowerCase());
+        }
     } else {
         value = String(element[field]).toLowerCase();
-        return value.includes(query.toLowerCase());
+        if (exactMatch) {
+            return value == query.toLowerCase();
+        } else {
+            return value.includes(query.toLowerCase());
+        }
     }
 }
 
-async function getTable(q, f) {
+// Determine max id and increase by one
+async function nextId(table) {
+    var result = await pool.query('SELECT id \
+    FROM ' + table + ' \
+    ORDER BY id DESC \
+    LIMIT 1');
+    return result.rows[0].id + 1;
+}
+
+async function getLocationsTable(q, f, exactMatch) {
     var result = await pool.query('SELECT locations.*, \
         maintainers.id AS maintainer_id, maintainers.name AS maintainer_name, maintainers.street AS maintainer_street, \
         maintainers.city AS maintainer_city, maintainers.province AS maintainer_province, maintainers.country AS maintainer_country \
@@ -26,9 +43,9 @@ async function getTable(q, f) {
         ORDER BY locations.id');
     // Filter
     if (q) {
-        var filteredRows = result.rows.filter((el) => isIncluded(el, q, f));
+        var filteredRows = result.rows.filter((el) => isIncluded(el, q, f, exactMatch));
         var idSet = new Set()
-        for(var row of filteredRows) {
+        for (var row of filteredRows) {
             idSet.add(row.id)
         }
         return result.rows.filter((el) => idSet.has(el.id));
@@ -37,7 +54,7 @@ async function getTable(q, f) {
     }
 }
 
-async function getJson(q, f) {
+async function getLocations(q, f, exactMatch) {
     var result = await pool.query('SELECT JSON_AGG(tab) \
         FROM \
         (SELECT locations.*, JSON_AGG(maintainers.*) AS maintainers \
@@ -49,9 +66,9 @@ async function getJson(q, f) {
         ) AS tab;');
     // Filter
     if (q) {
-        var rows = await getTable(q, f)
+        var rows = await getLocationsTable(q, f, exactMatch)
         var idSet = new Set()
-        for(var row of rows) {
+        for (var row of rows) {
             idSet.add(row.id)
         }
         return result.rows[0].json_agg.filter((el) => idSet.has(el.id));
@@ -60,7 +77,82 @@ async function getJson(q, f) {
     }
 }
 
+async function getMaintainers() {
+    var result = await pool.query('SELECT JSON_AGG(tab) \
+        FROM \
+        (SELECT * \
+            FROM maintainers \
+        ) AS tab;');
+    return result.rows[0].json_agg;
+}
+
+async function deleteLocation(id) {
+    var result1 = await pool.query('DELETE \
+    FROM locations_maintainers \
+    WHERE location_id = $1;',
+        [id]);
+    var result2 = await pool.query('DELETE \
+        FROM locations \
+        WHERE id = $1;',
+        [id]);
+}
+
+async function createLocation(locationDto, maintainerIds) {
+
+    // Determine id for location
+    var locationId = await nextId('locations')
+
+    // Insert location into locations
+    var result1 = await pool.query('INSERT INTO locations \
+    VALUES ($1, $2, $3, $4, $5, $6, $7)',
+        [locationId, locationDto.name, locationDto.natural_source, locationDto.geolocation_latitude, locationDto.geolocation_longitude,
+            locationDto.geolocation_altitude, locationDto.year_of_opening])
+
+    // Determine id for locations_maintainers relation
+    var relId = await nextId('locations_maintainers');
+
+    // Insert relations into locations_maintainers
+    for (var i in maintainerIds) {
+        var result2 = await pool.query('INSERT INTO locations_maintainers \
+        VALUES ($1, $2, $3)',
+            [relId + i, locationId, maintainerIds[i]]);
+    }
+    return locationId;
+}
+
+async function updateLocation(id, locationDto) {
+    var result = await pool.query('UPDATE locations \
+    SET (name, natural_source, geolocation_latitude, geolocation_longitude, geolocation_altitude, year_of_opening) = \
+    ($1, $2, $3, $4, $5, $6) \
+    WHERE id = $7',
+        [locationDto.name, locationDto.natural_source, locationDto.geolocation_latitude, locationDto.geolocation_longitude,
+        locationDto.geolocation_altitude, locationDto.year_of_opening, id]);
+}
+
+async function updateLocationMaintainers(locationId, maintainerIds) {
+    // Delete old locations_maintainers relations
+    var result1 = await pool.query('DELETE \
+    FROM locations_maintainers \
+    WHERE location_id = $1',
+        [locationId]);
+
+    // Determine id for locations_maintainers relation
+    var relId = await nextId('locations_maintainers');
+
+    // Insert relations into locations_maintainers
+    for (var i in maintainerIds) {
+        var result2 = await pool.query('INSERT INTO locations_maintainers \
+    VALUES ($1, $2, $3)',
+            [relId + i, locationId, maintainerIds[i]]);
+    }
+}
+
 module.exports = {
-    getTable,
-    getJson,
+    getLocationsTable,
+    getLocations,
+    getMaintainers,
+    deleteLocation,
+    createLocation,
+    updateLocation,
+    updateLocationMaintainers,
 }  
--- backend/index.js
+++ backend/index.js
@@ -16,16 +16,41 @@ app.use(
   })
 )
 
+class ResponseWrapper {
+  constructor(status, message, response) {
+    this.status = status;
+    this.message = message;
+    this.response = response;
+  }
+}
+
+class LocationDTO {
+  constructor(data) {
+    this.name = data.name;
+    this.natural_source = data.natural_source;
+    this.geolocation_latitude = data.geolocation_latitude;
+    this.geolocation_longitude = data.geolocation_longitude;
+    this.geolocation_altitude = data.geolocation_altitude;
+    this.year_of_opening = data.year_of_opening;
+  }
+}
+
+const NotImplementedWrapper = new ResponseWrapper("Not Implemented", "Method not supported by this endpoint.", null);
+const NotFoundIdWrapper = new ResponseWrapper("Not Found", "Resource with desired ID not found", null);
+const InternalErrorWrapper = new ResponseWrapper("Internal Server Error", "Error on server side", null);
+
 app.get('/', (request, response) => {
   response.json({ "/": 'Backend index', "/locations": "Locations of free drinking water" })
 })
 
-app.get('/locations', async (request, response) => {
-  response.status(200).json(await db.getTable(request.query.q, request.query.f));
+// Get filtered locations as table
+app.get('/locations/table', async (request, response) => {
+  response.status(200).json(await db.getLocationsTable(request.query.q, request.query.f));
 })
 
+// Get filtered locations as CSV
 app.get('/locations/csv', async (request, response) => {
-  const rows = await db.getTable(request.query.q, request.query.f)
+  const rows = await db.getLocationsTable(request.query.q, request.query.f)
   try {
     const parser = new Parser();
     const csv = parser.parse(rows);
@@ -36,10 +61,110 @@ app.get('/locations/csv', async (request, response) => {
   }
 })
 
+// Get locations filtered as JSON
 app.get('/locations/json', async (request, response) => {
-  const elements = await db.getJson(request.query.q, request.query.f)
+  const elements = await db.getLocations(request.query.q, request.query.f)
   response.attachment('locations_filtered.json');
   response.status(200).json(elements);
+})
+
+app.route('/locations')
+  // Get all locations
+  .get(async (request, response) => {
+    const elements = await db.getLocations();
+    const responseWrap = new ResponseWrapper("OK", "Fetched list of locations.", elements);
+    response.status(200).json(responseWrap);
+  })
+  // Create new location
+  .post(async (request, response) => {
+    const id = await db.createLocation(new LocationDTO(request.body), request.body.maintainer_ids);
+    const elements = await db.getLocations(id.toString(), "id", true);
+    if (elements.length == 1) {
+      response.status(201).json(new ResponseWrapper("Created", "Created new location", elements[0]));
+    } else {
+      response.status(500).json(InternalErrorWrapper);
+    }
+  })
+  .all((request, response) => {
+    response.status(501).json(NotImplementedWrapper);
+  })
+
+app.route('/locations/:id')
+  // Get specific location
+  .get(async (request, response) => {
+    const elements = await db.getLocations(request.params.id, "id", true);
+    if (elements.length == 1) {
+      const responseWrap = new ResponseWrapper("OK", "Fetched desired location.", elements[0]);
+      response.status(200).json(responseWrap);
+    } else if (elements.length < 1) {
+      response.status(404).json(NotFoundIdWrapper);
+    } else {
+      response.status(500).json(InternalErrorWrapper);
+    }
+  })
+  // Update specific location
+  .put(async (request, response) => {
+    const elements = await db.getLocations(request.params.id, "id", true);
+    if (elements.length == 1) {
+      await db.updateLocation(request.params.id, new LocationDTO(request.body));
+      const new_elements = await db.getLocations(request.params.id, "id", true);
+      response.status(200).json(new ResponseWrapper("OK", "Location updated.", new_elements[0]))
+    } else if (elements.length < 1) {
+      response.status(404).json(NotFoundIdWrapper);
+    } else {
+      response.status(500).json(InternalErrorWrapper);
+    }
+  })
+  // Delete specific location
+  .delete(async (request, response) => {
+    // Make sure location exists
+    const elements = await db.getLocations(request.params.id, "id", true);
+    if (elements.length == 1) {
+      await db.deleteLocation(request.params.id);
+      response.status(200).json(new ResponseWrapper("OK", "Location deleted.", elements[0]));
+    } else if (elements.length < 1) {
+      response.status(404).json(NotFoundIdWrapper);
+    } else {
+      response.status(500).json(InternalErrorWrapper);
+    }
+  })
+  .all((request, response) => {
+    response.status(501).json(NotImplementedWrapper);
+  })
+
+app.route('/locations/:id/maintainers')
+  // Update maintainers for location
+  .put(async (request, response) => {
+    const elements = await db.getLocations(request.params.id, "id", true);
+    if (elements.length == 1) {
+      await db.updateLocationMaintainers(request.params.id, request.body.maintainer_ids);
+      const new_elements = await db.getLocations(request.params.id, "id", true);
+      response.status(200).json(new ResponseWrapper("OK", "Location maintainers updated.", new_elements[0]))
+    } else if (elements.length < 1) {
+      response.status(404).json(NotFoundIdWrapper);
+    } else {
+      response.status(500).json(InternalErrorWrapper);
+    }
+  })
+  .all((request, response) => {
+    response.status(501).json(NotImplementedWrapper);
+  })
+
+app.route('/maintainers')
+  // Get all maintainers
+  .get(async (request, response) => {
+    const elements = await db.getMaintainers();
+    const responseWrap = new ResponseWrapper("OK", "Fetched list of maintainers.", elements);
+    response.status(200).json(responseWrap);
+  })
+  .all((request, response) => {
+    response.status(501).json(NotImplementedWrapper);
+  })
+
+
+// Other endpoints
+app.use((request, response, next) => {
+  response.status(404).json(new ResponseWrapper("Not Found", "Endpoint does not exist.", null));
 })
 
 app.listen(port, () => {
